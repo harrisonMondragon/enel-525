80f3aa10d383351bec3c122f4818215c31c8a21b
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-10-25 12:25:35 -0600

    Added table for part 1
    Might be wrong

--- labs/lab3/lab3_part1.py
+++ labs/lab3/lab3_part1.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from prettytable import PrettyTable
 
 # Given mean squared error function
 def mse(array):
@@ -43,3 +44,52 @@ plt.semilogy(mse_list)
 plt.xlabel("Iteration")
 plt.ylabel("MSE Value")
 plt.show()
+
+trained_list = []
+for j in range(len(p_array)):
+    trained_list.append(np.matmul(W, p_array[j]) + b)
+
+mse11 = mse(t_array[0]-trained_list[0])
+mse12 = mse(t_array[0]-trained_list[1])
+mse13 = mse(t_array[0]-trained_list[2])
+mse14 = mse(t_array[0]-trained_list[3])
+mse15 = mse(t_array[0]-trained_list[4])
+mse16 = mse(t_array[0]-trained_list[5])
+mse17 = mse(t_array[0]-trained_list[6])
+mse18 = mse(t_array[0]-trained_list[7])
+
+mse21 = mse(t_array[2]-trained_list[0])
+mse22 = mse(t_array[2]-trained_list[1])
+mse23 = mse(t_array[2]-trained_list[2])
+mse24 = mse(t_array[2]-trained_list[3])
+mse25 = mse(t_array[2]-trained_list[4])
+mse26 = mse(t_array[2]-trained_list[5])
+mse27 = mse(t_array[2]-trained_list[6])
+mse28 = mse(t_array[2]-trained_list[7])
+
+mse31 = mse(t_array[4]-trained_list[0])
+mse32 = mse(t_array[4]-trained_list[1])
+mse33 = mse(t_array[4]-trained_list[2])
+mse34 = mse(t_array[4]-trained_list[3])
+mse35 = mse(t_array[4]-trained_list[4])
+mse36 = mse(t_array[4]-trained_list[5])
+mse37 = mse(t_array[4]-trained_list[6])
+mse38 = mse(t_array[4]-trained_list[7])
+
+mse41 = mse(t_array[6]-trained_list[0])
+mse42 = mse(t_array[6]-trained_list[1])
+mse43 = mse(t_array[6]-trained_list[2])
+mse44 = mse(t_array[6]-trained_list[3])
+mse45 = mse(t_array[6]-trained_list[4])
+mse46 = mse(t_array[6]-trained_list[5])
+mse47 = mse(t_array[6]-trained_list[6])
+mse48 = mse(t_array[6]-trained_list[7])
+
+mse_table = PrettyTable()
+mse_table.field_names = ["", "P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"]
+mse_table.add_row(["Class1", mse11,mse12,mse13,mse14,mse15,mse16,mse17,mse18])
+mse_table.add_row(["Class2", mse21,mse22,mse23,mse24,mse25,mse26,mse27,mse28])
+mse_table.add_row(["Class3", mse31,mse32,mse33,mse34,mse35,mse36,mse37,mse38])
+mse_table.add_row(["Class4", mse41,mse42,mse43,mse44,mse45,mse46,mse47,mse48])
+
+print(mse_table)
