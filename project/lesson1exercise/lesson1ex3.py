51f50ffb75db71ec25304a6dac4c746346da6d84
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2023-11-24 05:34:05 +0700

    [Proj] Finish first project lesson 1 exercise
    This should be all I need for the project lesson 1 exercise

--- project/lesson1exercise/lesson1ex3.py
+++ project/lesson1exercise/lesson1ex3.py
@@ -1,48 +1,12 @@
-"""--------Exercise 2--------"""
-
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-# # Creating the dataset
+# Creating the dataset
 num_samples = 1000
 num_ft = 2
 
-# a = [10,5]*np.random.randn(num_samples,num_ft) + [150,50]
-# print(a.shape)
-
-# # Calculating the mean and std
-# m = np.mean(a, axis=0)
-# st = np.std(a, axis=0)
-# print(f"Mean = {m}")
-# print(f"std = {st}")
-
-# # Scatter plot
-# plt.scatter(a[:,0],a[:,1])
-# plt.title("Dataset a")
-# plt.xlabel("Height")
-# plt.ylabel("Weight")
-# plt.show()
-
-# # Histogram plot
-# plt.hist(a[:,0])
-# plt.title("Height Histogram")
-# plt.xlabel("Height")
-# plt.ylabel("Number of Samples")
-# plt.show()
-
-# # Standardizing the dataset
-# standard_a = (a-m)/st
-
-# plt.scatter(standard_a[:,0],standard_a[:,1])
-# plt.title("Standardized Dataset a")
-# plt.xlabel("Height")
-# plt.ylabel("Weight")
-# plt.show()
-
-
 """--------Exercise 3--------"""
-#    std                      dimensions        normal dist
-# a = [10,5]*np.random.randn(num_samples,num_ft) + [150,50]
 adult = [10,5]*np.random.randn(num_samples,num_ft) + [150,50]
 child = [10,5]*np.random.randn(num_samples,num_ft) + [50,25]
 dataset_array = np.concatenate((adult, child), axis=0)
@@ -57,3 +21,19 @@ plt.title("Lesson 1 Exercise 3 Dataset")
 plt.xlabel("Height (cm)")
 plt.ylabel("Weight (kg)")
 plt.show()
+
+# Convert to dataframes
+dataset = pd.DataFrame(dataset_array)
+targets = pd.DataFrame(targets_array)
+
+# Export as csv files
+dataset.to_csv("dataset.csv", index=False)
+targets.to_csv("targets.csv", index=False)
+
+# Read csv files
+check_dataset = pd.read_csv("dataset.csv")
+check_targets = pd.read_csv("targets.csv")
+
+# Print the read in csv files
+print(f"Dataset read from csv file:\n\n{check_dataset}")
+print(f"Targets read from csv file:\n\n{check_targets}")
